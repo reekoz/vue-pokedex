8a6a16b0c8d484bf8d36e2b135c187886d9d3f6f
Mattia Thiella <mattia.thiella@qualitas.it>
2021-05-07 15:22:43 +0200

    add colored stats

--- src/components/pokemons/PokemonStats.vue
+++ src/components/pokemons/PokemonStats.vue
@@ -3,27 +3,39 @@
     <base-card>
       <h5>
         ❤️ {{ stats[0].stat.name }}
-        <span class="stat">{{ stats[0].base_stat }}</span>
+        <span class="stat" :class="getStatColor(stats[0].base_stat)">{{
+          stats[0].base_stat
+        }}</span>
       </h5>
       <h5>
         ⚔️ {{ stats[1].stat.name }}
-        <span class="stat">{{ stats[1].base_stat }}</span>
+        <span class="stat" :class="getStatColor(stats[1].base_stat)">{{
+          stats[1].base_stat
+        }}</span>
       </h5>
       <h5>
         🛡️ {{ stats[2].stat.name }}
-        <span class="stat">{{ stats[2].base_stat }}</span>
+        <span class="stat" :class="getStatColor(stats[2].base_stat)">{{
+          stats[2].base_stat
+        }}</span>
       </h5>
       <h5>
         🔮 {{ stats[3].stat.name }}
-        <span class="stat">{{ stats[3].base_stat }}</span>
+        <span class="stat" :class="getStatColor(stats[3].base_stat)">{{
+          stats[3].base_stat
+        }}</span>
       </h5>
       <h5>
         🔰 {{ stats[4].stat.name }}
-        <span class="stat">{{ stats[4].base_stat }}</span>
+        <span class="stat" :class="getStatColor(stats[4].base_stat)">{{
+          stats[4].base_stat
+        }}</span>
       </h5>
       <h5>
         ⚡️ {{ stats[5].stat.name }}
-        <span class="stat">{{ stats[5].base_stat }}</span>
+        <span class="stat" :class="getStatColor(stats[5].base_stat)">{{
+          stats[5].base_stat
+        }}</span>
       </h5>
     </base-card>
   </section>
@@ -32,7 +44,21 @@
 <script>
 export default {
   props: ['stats'],
-  setup() {}
+  setup() {
+    const getStatColor = value => {
+      if (value < 15) return 'pomegranate';
+      if (value < 24) return 'cabaret';
+      if (value < 42) return 'alzarin';
+      if (value < 55) return 'orange';
+      if (value < 73) return 'sunflower';
+      if (value < 85) return 'emerald';
+      if (value < 97) return 'turquoise';
+      if (value < 110) return 'greensea';
+      return 'peterriver';
+    };
+
+    return { getStatColor };
+  }
 };
 </script>
 
@@ -46,5 +72,41 @@ h5 {
   font-size: 1.15rem;
   float: right;
   margin-left: 1rem;
+}
+
+.pomegranate {
+  color: #c0392b;
+}
+
+.cabaret {
+  color: #d2527f;
+}
+
+.alzarin {
+  color: #e74c3c;
+}
+
+.orange {
+  color: #ff851b;
+}
+
+.sunflower {
+  color: #f1c40f;
+}
+
+.emerald {
+  color: #2ecc71;
+}
+
+.turquoise {
+  color: #1abc9c;
+}
+
+.greensea {
+  color: #16a085;
+}
+
+.peterriver {
+  color: #3498db;
 }
 </style>
